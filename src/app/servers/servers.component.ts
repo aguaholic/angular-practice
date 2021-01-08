85e27e516c087d9e6b5d2dc7dd2fcf50ab1888a8
aguaholic <joyterra@gmail.com>
2021-01-08 17:47:59 +0100

    Implement ngFor - structural directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName = '';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
 
   constructor() {
     setTimeout(() => {
@@ -19,6 +20,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created. Name is' + ' ' + this.serverName;
   }
